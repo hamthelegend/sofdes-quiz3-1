c818b37986cf088d95b422d79780cc47b84e68fa
hamthelegend <hamuelxd@gmail.com>
2022-05-24 10:24:28 +0800

    Add project files.

---
+++ SofdesQuiz3-1/MainWindow.xaml.cs
@@ -0,0 +1,163 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+
+namespace SofdesQuiz3_1
+{
+    public sealed partial class MainWindow : Window
+    {
+        private List<User> Users;
+        public MainWindow()
+        {
+            InitializeComponent();
+            LoadData();
+            genderInput.SelectedItem = "Unspecified";
+        }
+
+        private void Clear(object sender, RoutedEventArgs e)
+        {
+            Clear();
+        }
+
+        private async void Remove(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(idInput.Text))
+            {
+                await new ContentDialog
+                {
+                    Title = "No user selected",
+                    Content = "Select the user you want to delete from the list first.",
+                    CloseButtonText = "Okay",
+                    XamlRoot = Content.XamlRoot,
+                }.ShowAsync();
+                return;
+            }
+
+            var response = await new ContentDialog
+            {
+                Title = "Delete user",
+                Content = "Are you sure you want to delete this user?",
+                PrimaryButtonText = "Yes",
+                CloseButtonText = "No",
+                XamlRoot = Content.XamlRoot,
+            }.ShowAsync();
+
+            if (response == ContentDialogResult.Primary)
+            {
+                var id = int.Parse(idInput.Text);
+                UsersDb.Delete(id);
+                Clear();
+                LoadData();
+            }
+        }
+
+        private async void AddUpdate(object sender, RoutedEventArgs e)
+        {
+            var user = await ParseUserAsync();
+            if (user != null)
+            {
+                UsersDb.InsertUpdateAsync(user);
+                Clear();
+                LoadData();
+            }
+        }
+
+        private void Search(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SelectUser(object sender, DoubleTappedRoutedEventArgs e)
+        {
+            var user = usersDataGrid.SelectedItem as User;
+            if (user != null)
+            {
+                idInput.Text = user.Id.ToString();
+                fullNameInput.Text = user.FullName;
+                emailInput.Text = user.Email;
+                birthdateInput.SelectedDate = user.Birthdate;
+                genderInput.Text = user.Gender;
+                addressInput.Text = user.Address;
+            }
+        }
+
+        private async Task<User> ParseUserAsync()
+        {
+            int? id;
+
+            if (string.IsNullOrEmpty(idInput.Text))
+            {
+                id = null;
+            }
+            else
+            {
+                id = int.Parse(idInput.Text);
+            }
+
+            var fullName = fullNameInput.Text;
+            var email = emailInput.Text;
+            var birthdate = birthdateInput.SelectedDate;
+            var gender = genderInput.SelectedItem as string;
+            var address = addressInput.Text;
+
+            if (string.IsNullOrEmpty(fullName) ||
+                string.IsNullOrEmpty(email) ||
+                birthdate == null ||
+                gender == null ||
+                string.IsNullOrEmpty(address))
+            {
+                await new ContentDialog
+                {
+                    Title = "Empty fields",
+                    Content = "None of the fields can be empty.",
+                    CloseButtonText = "Okay",
+                    XamlRoot = Content.XamlRoot,
+                }.ShowAsync();
+                return null;
+            }
+            return new User(fullName, email, (DateTimeOffset)birthdate, gender, address, id);
+        }
+
+        private void Clear()
+        {
+            idInput.Text = string.Empty;
+            fullNameInput.Text = string.Empty;
+            emailInput.Text = string.Empty;
+            birthdateInput.SelectedDate = null;
+            genderInput.SelectedItem = "Unspecified";
+            addressInput.Text = string.Empty;
+            usersDataGrid.SelectedItem = null;
+        }
+
+        private void LoadData()
+        {
+            Users = UsersDb.GetAll();
+        }
+
+        private void idInput_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(idInput.Text))
+            {
+                addButton.Content = "Add";
+            }
+            else
+            {
+                addButton.Content = "Update";
+            }
+        }
+
+    }
+}
---
+++ SofdesQuiz3-1/User.cs
@@ -0,0 +1,129 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SofdesQuiz3_1;
+public class User
+{
+    public User(string fullName, string email, DateTimeOffset birthdate, string gender, string address, int? id = null)
+    {
+        Id = id;
+        FullName = fullName;
+        Email = email;
+        Birthdate = birthdate;
+        Gender = gender;
+        Address = address;
+    }
+
+    public int? Id { get; }
+    public string FullName { get; }
+    public string Email { get; }
+    public DateTimeOffset Birthdate { get; }
+    public string Gender { get; }
+    public string Address { get; }
+
+    public UserEntity ToUserEntity()
+    {
+        var userEntity = new UserEntity()
+        {
+            FullName = FullName,
+            Email = Email,
+            Birthdate = Birthdate,
+            Gender = Gender,
+            Address = Address,
+        };
+        if (Id != null) userEntity.Id = (int)Id;
+        return userEntity;
+    }
+
+}
+
+[Table("UsersTable")]
+public class UserEntity
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+    public string FullName { get; set; }
+    public string Email { get; set; }
+    public DateTimeOffset Birthdate { get; set; }
+    public string Gender { get; set; }
+    public string Address { get; set; }
+    
+    public User ToUser()
+    {
+        return new User(FullName, Email, Birthdate, Gender, Address, Id);
+    }
+}
+
+public class UsersContext : DbContext
+{
+    public DbSet<UserEntity> UserEntities { get; set; }
+
+    public string DbPath { get; }
+
+    public UsersContext()
+    {
+        var folder = Environment.SpecialFolder.LocalApplicationData;
+        var path = Environment.GetFolderPath(folder);
+        DbPath = System.IO.Path.Join(path, "users.db");
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options) => options.UseSqlite($"Data Source={DbPath}");
+
+}
+
+public static class UsersDb
+{
+    public static User Get(int id)
+    {
+        var context = new UsersContext();
+        var userEntity = context.UserEntities.Where(user => user.Id == id).FirstOrDefault();
+        return userEntity?.ToUser();
+    }
+    public static List<User> GetAll()
+    {
+        var context = new UsersContext();
+        var userEntities = context.UserEntities.ToList();
+        return userEntities.Select(userEntity => userEntity.ToUser()).ToList();
+    }
+
+    public static async void InsertUpdateAsync(User user)
+    {
+        var context = new UsersContext();
+        var userEntityOnDb = context.UserEntities.Where(userEntity => userEntity.Id == user.Id).FirstOrDefault();
+        if (userEntityOnDb == null)
+        {
+            var userEntity = user.ToUserEntity();
+            context.Add(userEntity);
+            context.SaveChanges();
+        }
+        else
+        {
+            userEntityOnDb.FullName = user.FullName;
+            userEntityOnDb.Email = user.Email;
+            userEntityOnDb.Birthdate = user.Birthdate;
+            userEntityOnDb.Gender = user.Gender;
+            userEntityOnDb.Address = user.Address;
+            context.SaveChanges();
+        }
+    }
+
+    public static bool Delete(int id)
+    {
+        var context = new UsersContext();
+        var userEntityOnDb = context.UserEntities.Where(userEntity => userEntity.Id == id).FirstOrDefault();
+        if (userEntityOnDb == null)
+        {
+            return false;
+        }
+        context.Remove(userEntityOnDb);
+        context.SaveChanges();
+        return true;
+    }
+}
